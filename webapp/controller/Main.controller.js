e7205ed10b715a0441524b5392946780f048b1b8
LUIZ GUNI SANTOS <166174515+Luizguni@users.noreply.github.com>
2025-11-22 19:08:06 -0300

    Nova implementação da API

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,10 +1,18 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel"],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   * @param {typeof sap.ui.model.json.JSONModel} JSONModel
+   */
+  function (Controller, JSONModel) {
     "use strict";
 
     return Controller.extend("megaacademy.mod2aula2.controller.Main", {
-        onInit() {
-        }
+      onInit: function () {
+        //let oModelLocal = new JSONModel();
+        let oModelglobal = this.getOwnerComponent().getModel();
+        oModelglobal.loadData("http://swapi.dev/api/people/");
+        //this.getView().setModel(oModelLocal);
+      },
     });
-});
+  });
